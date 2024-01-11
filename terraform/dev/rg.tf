d1bff838061420e06e2dff69a0601205ef9942e8
AMIT CHATURVEDI <65268854+gittest20202@users.noreply.github.com>
2024-01-11 21:02:34 +0530

    updaed the rg.tg file

--- terraform/dev/rg.tf
+++ terraform/dev/rg.tf
@@ -6,5 +6,5 @@ resource "azurerm_virtual_network" "myvnet" {
   name                = "${var.vnetname}"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.myrg.location
-  resource_group_name = azurerm_resource_group_myrg.name
+  resource_group_name = azurerm_resource_group.myrg.name
 }
